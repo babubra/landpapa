318f2f21fa358efcb939ac2661f7b279fe0e9e64
babubra <aiplegal60@gmail.com>
2026-01-19 11:12:08 +0200

    feat: добавлен счётчик Яндекс.Метрики с поддержкой SPA

--- kaliningrad-land/src/app/layout.tsx
+++ kaliningrad-land/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Montserrat, Manrope } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SiteSettingsProvider } from "@/contexts/SiteSettingsContext";
@@ -90,6 +91,24 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} ${manrope.variable} antialiased font-sans`}
       >
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106326739', 'ym');
+
+            ym(106326739, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/106326739" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
         <SiteNavigationSchema />
         <ThemeProvider
           attribute="class"
